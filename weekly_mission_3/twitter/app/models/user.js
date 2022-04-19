72027c6efc1668714d851f54acfea75b7e2a01f0
Drako9159 <chochito9159@gmail.com>
2022-04-19 15:13:28 -0500

    twitter file completed

--- weekly_mission_3/twitter/app/models/user.js
+++ weekly_mission_3/twitter/app/models/user.js
@@ -1,4 +1,6 @@
 class User{
+    
+    /*Requerimiento 1: Se necesita un modelo `User` para guardar la información de un usuario de nuestra app: `id`, `username`, `name`, `bio`, `dateCreated` y `lastUpdated`. 
     constructor(id, username, name, bio, dateCreated, lastUpdated){
         this.id = id
         this.username = username
@@ -6,6 +8,35 @@ class User{
         this.bio = bio
         this.dateCreated = dateCreated
         this.lastUpdated = lastUpdated
+    }*/
+    /*Requerimiento 2: Las propiedades `dateCreated` y `lastUpdated` deberán ser datos de tipo fecha que guarden el momento en que se instancie un nuevo objeto de la clase `User`*/
+    constructor(id, username, name, bio){
+        this.id = id
+        this.username = username
+        this.name = name
+        this.bio = bio
+        this.dateCreated = new Date()
+        this.lastUpdated = new Date()
+    }
+    /*Requerimiento 3: getters*/
+    get getUsername(){
+        return this.username
+    }
+    get getBio(){
+        return this.bio
+    }
+    get getDateCreated(){
+        return this.dateCreated
+    }
+    get getLastUpdated(){
+        return this.lastUpdated
+    }
+    //Requerimiento 5:
+    set setUsername(newUsername){
+        this.username = newUsername
+    }
+    set setBio(newBio){
+        this.bio = newBio
     }
 }
 module.exports = User
--- weekly_mission_3/twitter/test/models/user.test.js
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -1,5 +1,5 @@
 const User = require('../../app/models/user.js')
-
+/*Requerimento 1
 describe("Unit tests for user class", () => {
     test('Create an User objet', () => {
         //Aquí se invoca el código que se va a usar en nuestra app
@@ -11,5 +11,46 @@ describe("Unit tests for user class", () => {
         expect(user.bio).toBe("Bio")
         expect(user.dateCreated).toBe("dateCreated")
         expect(user.lastUpdated).toBe("lastUpdated")
+    })
+})*/
+/*Requerimento 2
+describe("Unit tests for user class", () => {
+    test('Create an User objet', () => {
+        //Aquí se invoca el código que se va a usar en nuestra app
+        const user = new User(1, "drako9159", "Antonio", "Bio")
+        //Se validan los resultados
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("drako9159")
+        expect(user.name).toBe("Antonio")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).not.toBeUndefined() //Verifica que el valor no se indefinido
+        expect(user.lastUpdated).not.toBeUndefined()
+    })
+})*/
+/*Requerimiento 3
+describe("Unit tests for user class", () => {
+    test('Add getters', () => {
+        //Aquí se invoca el código que se va a usar en nuestra app
+        const user = new User(1, "drako9159", "Antonio", "Bio")
+        //Se validan los resultados
+        expect(user.id).toBe(1)
+        expect(user.getUsername).toBe("drako9159")
+        expect(user.name).toBe("Antonio")
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined() //Verifica que el valor no se indefinido
+        expect(user.getLastUpdated).not.toBeUndefined()
+    })
+})*/
+/*Requerimiento 4*/
+describe("Unit tests for user class", () => {
+    test('Add getters', () => {
+        //Aquí se invoca el código que se va a usar en nuestra app
+        const user = new User(1, "drako9159", "Antonio", "Bio")
+        //Se validan los resultados
+        user.setUsername = "Drako"
+        expect(user.username).toBe("Drako")
+
+        user.setBio = "New bio"
+        expect(user.bio).toBe("New bio")
     })
 })
